8f25d328d8e222cd4f5d0f09b0f104a25e0da7b0
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2026-09-02 23:05:42 +0000

    adiciona funcionalidades de CRUD com eventos para salvar, ler, atualizar e deletar nome

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-// LÓGICA PRA SALVAR
+// Evento de clique no botão salvar
 btn = document.getElementById("btnSalvar")
-btn.addEventListener('click', () => {
+btn.addEventListener('click', salvar)
+
+// Evento de clique no botão Ler
+btn = document.getElementById("btnLer")
+btn.addEventListener('click', ler)
+
+// Evento de clique no botão Ler
+btn = document.getElementById("btnDeletar")
+btn.addEventListener('click', deletar)
+
+// Evento de clique no botão Atualizar
+btn = document.getElementById("btnAtualizar")
+btn.addEventListener('click', atualizar)
+
+function salvar() {
     name = document.getElementById("name").value
     localStorage.setItem("name", name)
-})
-// LÓGICA PRA LER
-btn = document.getElementById("btnLer")
-btn.addEventListener('click', () => {
+}
+
+// Função para ler dados
+function ler() {
     name = localStorage.getItem("name")
     document.querySelector("p").innerText = name
-})
+}
+
+// Função para atualizar dados
+function atualizar() {
+    salvar()
+    ler()
+}
+
+// Função para deletar dados
+function deletar() {
+    localStorage.removeItem("name")
+    document.querySelector("p").innerText = "deletado"
+}
